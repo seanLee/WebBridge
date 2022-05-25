feb7ceea08eec9d735c9e04e9418ddcbd8bab9ed
seanLee <seannnLee@163.com>
2022-05-25 15:58:58 +0800

    Release 0.0.2

---
+++ Example/MyTest.h
@@ -0,0 +1,17 @@
+//
+//  MyTest.h
+//  WebBridge_Tests
+//
+//  Created by Sean on 2022/5/25.
+//  Copyright © 2022 seanLee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyTest : NSObject
+- (void)call;
+@end
+
+NS_ASSUME_NONNULL_END
